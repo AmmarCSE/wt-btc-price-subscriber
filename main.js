5f660a401ef00d4ceeb056bc1c7c90136455d4b8
AmmarCSE <ammar.abu-yasein@farsightsoftware.com>
2017-02-17 17:53:09 +0300

    es6ify

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
+'use latest';
+
 module.exports = function (cb) { 
-    var http = require("http"),
+    const http = require("http"),
         client = http.createClient(80, "blockchain.info"),
-        querystring = require('querystring'),
-        query = "USD",
         request = client.request("GET", "/tobtc?currency=USD&value=1100", {'Host': 'blockchain.info'});
 
-    request.on('response', function (response) {
-        var body = '';
-        response.on('data', function (chunk) {
+    request.on('response', (response) => {
+        let body = '';
+        response.on('data', (chunk) => {
             body += chunk;
         });
-        response.on('end', function () {
+        response.on('end', () => {
             cb(null, body);
         });
     });
