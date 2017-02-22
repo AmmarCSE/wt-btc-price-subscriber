dee7f0a8152a93df0b21b7ac74df5a41bfe89edc
AmmarCSE <ammar.abu-yasein@farsightsoftware.com>
2017-02-22 20:14:21 +0300

    artifacts

--- subscribe.js
+++ subscribe.js
@@ -3,22 +3,22 @@
 const MongoClient = require('mongodb').MongoClient;
 
 export default function (ctx, done) {
-    let { high , low , currency , email } = ctx.data;
+    let { high , low , currency , username } = ctx.data;
 
     //need to coerce high/low below because they are in string form
-    const subscription = {  high: +high , low: +low , currency , email};
-    addSubscription(subscription, done, ctx.data.MONGO_URL);
-}
+    const subscription = {  high: +high , low: +low , currency , username};
+    addSubscription(subscription, done);
 
-function addSubscription(subscription, cb, mongoUrl) {
-    MongoClient.connect(mongoUrl, (err, db) => {
-        db.collection('subscriptions')
-            .insertOne(subscription, (err) => {
-                if(err){
-                    return cb(err);
-                }
+    function addSubscription(subscription, cb) {
+        MongoClient.connect(ctx.data.MONGO_URL, (err, db) => {
+            db.collection('subscriptions')
+                .insertOne(subscription, (err) => {
+                    if(err){
+                        return cb(err);
+                    }
 
-                cb(null);
-            });
-    });
+                    cb(null);
+                });
+        });
+    }
 }
